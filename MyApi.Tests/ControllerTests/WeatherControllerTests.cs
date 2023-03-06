afe4cba81ffc578e2e2182b207b69a1e3ec00e7d
BouchefaMahmoud <mahmoudbouchefa06@gmail.com>
2023-03-06 09:36:42 +0100

    add unit tests

---
+++ MyApi.Tests/ControllerTests/WeatherControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+using MyApi.Controllers;
+using Xunit;
+
+namespace MyApi.Tests.ControllerTests;
+
+public class WeatherControllerTests
+{
+  private Mock<ILogger<WeatherForecastController>> _mockLogger = new();
+  
+  [Fact]
+  public void GetWeatherForecast_ReturnsWeatherForecast()
+  {
+    var controller = new WeatherForecastController(_mockLogger.Object);
+    var result = controller.Get();
+    Assert.NotNull(result);
+  }
+  
+  [Fact]
+  public void GetWeatherForecast_ReturnsWeatherForecastWithFiveItems()
+  {
+    var controller = new WeatherForecastController(_mockLogger.Object);
+    var result = controller.Get();
+    Assert.Equal(5, result.Count());
+  }
+  
+}
